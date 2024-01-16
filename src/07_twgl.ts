01aacdb7e0bfaacd0d11b0705b4d695925d349c2
zzingo5 <zzingo5@gmail.com>
2024-01-17 08:20:34 +0900

    feat: twgl code 다시 처음부터 하기

--- src/07_twgl.ts
+++ src/07_twgl.ts
@@ -47,7 +47,7 @@ function main() {
     u_lightColor: [1, 1, 1, 1],
   };
 
-  const uniformsThatAreComputedForEachObject = {
+  const uniformsThatAreComputedForEachObjects = {
     u_worldViewProjection: twgl.m4.identity(),
     u_world: twgl.m4.identity(),
     u_worldInverseTranspose: twgl.m4.identity(),
@@ -65,116 +65,16 @@ function main() {
     return Math.floor(Math.random() * range);
   };
 
-  const textures = [
-    textureUtils.makeStripeTexture(gl, { color1: "#FFF", color2: "#CCC" }),
-    textureUtils.makeCheckerTexture(gl, { color1: "#FFF", color2: "#CCC" }),
-    textureUtils.makeCircleTexture(gl, { color1: "#FFF", color2: "#CCC" }),
-  ];
+  requestAnimationFrame(drawScene);
 
-  const objects = [];
-  const numObjects = 300;
-  const baseColor = rand(240);
-  for (let ii = 0; ii < numObjects; ++ii) {
-    objects.push({
-      radius: rand(150),
-      xRotation: rand(Math.PI * 2),
-      yRotation: rand(Math.PI),
-      materialUniforms: {
-        u_colorMult: chroma.hsv(rand(baseColor, baseColor + 120), 0.5, 1).gl(),
-        u_diffuse: textures[randInt(textures.length)],
-        u_specular: [1, 1, 1, 1],
-        u_shininess: rand(500),
-        u_specularFactor: rand(1),
-      },
-    });
+  // Draw the scene.
+  function drawScene(time: number) {
+    time = 5 + time * 0.0001;
+
+    twgl.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
+
+    requestAnimationFrame(drawScene);
   }
-
-  //requestAnimationFrame(drawScene);
-
-  // TODO: Draw the scene.
-  // function drawScene(time) {
-  //   time = 5 + time * 0.0001;
-
-  //   twgl.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
-
-  //   // Tell WebGL how to convert from clip space to pixels
-  //   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  //   gl.enable(gl.CULL_FACE);
-  //   gl.enable(gl.DEPTH_TEST);
-
-  //   // Compute the projection matrix
-  //   var aspect =
-  //     (gl.canvas as HTMLCanvasElement).clientWidth /
-  //     (gl.canvas as HTMLCanvasElement).clientHeight;
-  //   var projectionMatrix = twgl.m4.perspective(
-  //     fieldOfViewRadians,
-  //     aspect,
-  //     1,
-  //     2000
-  //   );
-
-  //   // Compute the camera's matrix using look at.
-  //   var cameraPosition = [0, 0, 100];
-  //   var target = [0, 0, 0];
-  //   var up = [0, 1, 0];
-  //   var cameraMatrix = twgl.m4.lookAt(
-  //     cameraPosition,
-  //     target,
-  //     up,
-  //     uniformsThatAreTheSameForAllObjects.u_viewInverse
-  //   );
-
-  //   // Make a view matrix from the camera matrix.
-  //   var viewMatrix = twgl.m4.inverse(cameraMatrix);
-
-  //   var viewProjectionMatrix = twgl.m4.multiply(projectionMatrix, viewMatrix);
-
-  //   gl.useProgram(program);
-
-  //   // Setup all the needed attributes.
-  //   gl.bindVertexArray(vao);
-
-  //   // Set the uniforms that are the same for all objects.
-  //   twgl.setUniforms(uniformSetters, uniformsThatAreTheSameForAllObjects);
-
-  //   // Draw objects
-  //   objects.forEach(function (object) {
-  //     // Compute a position for this object based on the time.
-  //     var worldMatrix = twgl.m4.identity();
-  //     worldMatrix = twgl.m4.rotateY(worldMatrix, object.yRotation * time);
-  //     worldMatrix = twgl.m4.rotateX(worldMatrix, object.xRotation * time);
-  //     worldMatrix = twgl.m4.translate(
-  //       worldMatrix,
-  //       0,
-  //       0,
-  //       object.radius,
-  //       uniformsThatAreComputedForEachObject.u_world
-  //     );
-
-  //     // Multiply the matrices.
-  //     twgl.m4.multiply(
-  //       viewProjectionMatrix,
-  //       worldMatrix,
-  //       uniformsThatAreComputedForEachObject.u_worldViewProjection
-  //     );
-  //     twgl.m4.transpose(
-  //       twgl.m4.inverse(worldMatrix),
-  //       uniformsThatAreComputedForEachObject.u_worldInverseTranspose
-  //     );
-
-  //     // Set the uniforms we just computed
-  //     twgl.setUniforms(uniformSetters, uniformsThatAreComputedForEachObject);
-
-  //     // Set the uniforms that are specific to the this object.
-  //     twgl.setUniforms(uniformSetters, object.materialUniforms);
-
-  //     // Draw the geometry.
-  //     gl.drawElements(gl.TRIANGLES, buffers.numElements, gl.UNSIGNED_SHORT, 0);
-  //   });
-
-  //   requestAnimationFrame(drawScene);
-  // }
 }
 
 main();
